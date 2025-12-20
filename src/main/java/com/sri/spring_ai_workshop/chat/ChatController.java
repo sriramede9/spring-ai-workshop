5193f4095267f0af02dcc1aafa2adcc24980016c
“sriramede9” <sriramede@sris-mac-mini.home>
2025-12-19 19:57:53 -0500

    spring-ai: moved to gemini ai for flexibility

--- src/main/java/com/sri/spring_ai_workshop/chat/ChatController.java
+++ src/main/java/com/sri/spring_ai_workshop/chat/ChatController.java
@@ -4,7 +4,9 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -17,10 +19,16 @@ public class ChatController {
         this.chatClient = chatClient.build();
     }
 
-    @GetMapping("/{prompt}")
-    public ResponseEntity<String> chat(String prompt) {
-        String content = chatClient.prompt(prompt).call().content();
+    @GetMapping("/chat/{prompt}")
+    public ResponseEntity<String> chat(@PathVariable String prompt) {
+        String content = chatClient.prompt().user(prompt).call().content();
         return org.springframework.http.ResponseEntity.ok(content);
     }
 
+    @GetMapping("/stream/{prompt}")
+    public ResponseEntity<Flux<String>> stream(@PathVariable String prompt) {
+        Flux<String> content = chatClient.prompt().user(prompt)
+                .stream().content();
+        return org.springframework.http.ResponseEntity.ok(content);
+    }
 }
